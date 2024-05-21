844b8a9165911f75488b284ebbf3a835c5659af5
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-05-21 07:57:01 +0000

    Add get_user_input() function

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+import pandas as pd
+from io import BytesIO
+
+def get_user_input():
+    print("You need to put in the url of a raw file in the format CSV")
+    url = input('Put in your URL here:\n')
+
+    response = requests.get(url)
+
+    raw_data = pd.read_csv(BytesIO(response.content))
+    return(raw_data)
