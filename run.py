10730679e58bfd1f8ff603a436e806ea9c3f2d67
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-05-24 14:51:48 +0000

    Add option to quit program in main menu, change sub menu 'insert new file' to 'Back to main menu'

--- run.py
+++ run.py
@@ -42,17 +42,17 @@ calculated values.
             # URL or a timeout
             except requests.exceptions.RequestException as e:
                 print(f"Couldn't download file. Please make sure URL is correct: {e}")
-                return None
+                break
             
             # Displays error when file isn't correctly structured or data type is wrong
             except pd.errors.ParserError as e:
                 print(f"Couldn't read file. Please ensure the file is a CSV file and try again: {e}")
-                return None
+                break
             
             # Displays all errors that could happen and aren't catched before
             except Exception as e:
                 print(f"Unexpected error: {e}")
-                return None
+                break
 
 def check_data(data):
     """
@@ -214,40 +214,42 @@ def start_analysis(num_data, clean_data, list_products):
     question and product. It also provides a menu where the user can choose to 
     watch each analysis individually or to insert a new file for analysis
     """
-    #Deefine parameters for sub menu and back menu/button
-    sub_options = [
-        "[q] Analysis Questions", "[p] Analysis Products", 
-        "[n] Insert new File"
-        ]
-    back_sub = ["[b] Back to Sub Menu"]
-    sub_menu = TerminalMenu(sub_options)
-    back_sub_menu = TerminalMenu(back_sub)
-    # Show submenu for individual analysis
-    sub_menu_index = sub_menu.show()
-    sub_menu_choice = sub_options[sub_menu_index]
-    # Start analysis for questions
-    if sub_menu_choice == "[q] Analysis Questions":
-        print_legend()
-        calculate_mean_questions(num_data)
-        find_mode_questions(clean_data)
-        back_sub_index = back_sub_menu.show()
-        back_sub_choice = back_sub[back_sub_index]
-        # Offer option to go back to sub menu
-        if back_sub_choice == "[b] Back to Sub Menu":
-            start_analysis(num_data, clean_data, list_products)
-    # Start analysis for products
-    elif sub_menu_choice == "[p] Analysis Products":
-        print_legend()
-        calculate_mean_products(num_data, list_products)
-        find_mode_products(clean_data, list_products)
-        back_sub_index = back_sub_menu.show()
-        back_sub_choice = back_sub[back_sub_index]
-        # Offer option to go back to sub menu
-        if back_sub_choice == "[b] Back to Sub Menu":
-            start_analysis(num_data, clean_data, list_products)
-    # Goes back to main menu to insert new file
-    elif sub_menu_choice == "[n] Insert new File":
-        main()
+    quitting = False
+    while not quitting:
+        #Deefine parameters for sub menu and back menu/button
+        sub_options = [
+            "[q] Analysis Questions", "[p] Analysis Products", 
+            "[b] Back to Main Menu"
+            ]
+        back_sub = ["[b] Back to Sub Menu"]
+        sub_menu = TerminalMenu(sub_options)
+        back_sub_menu = TerminalMenu(back_sub)
+        # Show submenu for individual analysis
+        sub_menu_index = sub_menu.show()
+        sub_menu_choice = sub_options[sub_menu_index]
+        # Start analysis for questions
+        if sub_menu_choice == "[q] Analysis Questions":
+            print_legend()
+            calculate_mean_questions(num_data)
+            find_mode_questions(clean_data)
+            back_sub_index = back_sub_menu.show()
+            back_sub_choice = back_sub[back_sub_index]
+            # Offer option to go back to sub menu
+            if back_sub_choice == "[b] Back to Sub Menu":
+                start_analysis(num_data, clean_data, list_products)
+        # Start analysis for products
+        elif sub_menu_choice == "[p] Analysis Products":
+            print_legend()
+            calculate_mean_products(num_data, list_products)
+            find_mode_products(clean_data, list_products)
+            back_sub_index = back_sub_menu.show()
+            back_sub_choice = back_sub[back_sub_index]
+            # Offer option to go back to sub menu
+            if back_sub_choice == "[b] Back to Sub Menu":
+                start_analysis(num_data, clean_data, list_products)
+        # Goes back to main menu to insert new file
+        elif sub_menu_choice == "[b] Back to Main Menu":
+            quitting = True
 
 def main():
     """
@@ -259,52 +261,53 @@ def main():
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 Hello and welcome to the product-survey-analysis!
-If you already now how it works click on "Start"
-or press the "s" button on your keyboard.
-Should it be your first time, click on "Show Explanation"
-or press the "e" button on your keyboard.
+If you already now how it works click on "Start" or 
+press the "s" button on your keyboard.
+Should it be your first time, click on "Show Explanation" or 
+press the "e" button on your keyboard.
     """)
     #Definition of options for different terminal menus
-    main_options = ["[s] Start", "[e] Show Explanation"]
+    main_options = ["[s] Start", "[e] Show Explanation", "[q] Quit"]
     back_main = ["[b] Back to Main Menu"]
-    
-    # Declares the different terminal menus
-    main_menu = TerminalMenu(main_options)
-    back_main_menu = TerminalMenu(back_main)
+    quitting = False
+    while not quitting:
+        # Declares the different terminal menus
+        main_menu = TerminalMenu(main_options)
+        back_main_menu = TerminalMenu(back_main)    
+        # Starts the Main Menu 
+        main_menu_index = main_menu.show()
+        main_menu_choice = main_options[main_menu_index]
+        # Start the analysis
+        if main_menu_choice == "[s] Start":
+            # Get data from user and prepare data for analysis
+            raw_data = get_user_input()
+            clean_data = check_data(raw_data)
+            return_questions(clean_data) 
+            list_products = count_products(clean_data)
+            num_data = transform_data(clean_data)
+            #Analyze the given data
+            start_analysis(num_data, clean_data, list_products)
+        # Shows the explanation how to use the program properly
+        elif main_menu_choice == "[e] Show Explanation":
+            print(f"""
+    1. You need to put in the direct URL to your survey data, which should be
+        stored publicly aviable.
 
-    # Starts the Main Menu 
-    main_menu_index = main_menu.show()
-    main_menu_choice = main_options[main_menu_index]
-    # Start the analysis
-    if main_menu_choice == "[s] Start":
-        # Get data from user and prepare data for analysis
-        raw_data = get_user_input()
-        clean_data = check_data(raw_data)
-        return_questions(clean_data) 
-        list_products = count_products(clean_data)
-        num_data = transform_data(clean_data)
-        #Analyze the given data
-        start_analysis(num_data, clean_data, list_products)
-    # Shows the explanation how to use the program properly
-    elif main_menu_choice == "[e] Show Explanation":
-        print(f"""
-1. You need to put in the direct URL to your survey data, which should be
-    stored publicly aviable.
+    2. The link must provide the raw data, not the document to download.
+        You can use for example github or dropbox and click on "raw data" 
+        to open the right URL. 
 
-2. The link must provide the raw data, not the document to download.
-    You can use for example github or dropbox and click on "raw data" 
-    to open the right URL. 
-
-3. The file needs to be formatted as CSV (Comma-Separated Values).
-    After providing the file, the analysis will run and put out the 
-    calculated values.
-        """)
-        print_legend()
-        #Offer a menu/button to go back to main menu
-        back_main_menu_index = back_main_menu.show()
-        back_main_menu_choice = back_main[back_main_menu_index]
-        if back_main_menu_choice == "[b] Back to Main Menu":
-            main()
+    3. The file needs to be formatted as CSV (Comma-Separated Values).
+        After providing the file, the analysis will run and put out the 
+        calculated values.
+            """)
+            print_legend()
+            #Offer a menu/button to go back to main menu
+            back_main_menu_index = back_main_menu.show()
+            back_main_menu_choice = back_main[back_main_menu_index]
+        # Quits the program
+        elif main_menu_choice == "[q] Quit":
+            quitting = True
 
 if __name__=="__main__":
     main()
