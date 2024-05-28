7e3b3e624116b5a376bb9bcc0f46918913c6c995
Marlon Martynkiewitz <marlon.martynkiewitz@googlemail.com>
2024-05-28 07:15:02 +0000

    Delete whitespace and add linebreaks

---
+++ TESTING.md
@@ -0,0 +1,3 @@
+Click on the following link to get back to the [README.md](README.md).
+## Testing
+During the development of the program, every function was tested thoroughly to make sure everything worked as intended. Also, the code was passed through the Code Institute [PEP8 CI Python Linter](https://pep8ci.herokuapp.com/). 
--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from io import BytesIO
 import validators
 from simple_term_menu import TerminalMenu
 
+
 def get_user_input():
     """
     Gets the input from the user and checks for possible errors
@@ -13,46 +14,54 @@ def get_user_input():
     # Loops and asks for url until valid url is given as input
     while True:
         print(f"""
---------------------------------------------------------------------------------        
+--------------------------------------------------------------------------------
 Put in the URL for your raw CSV file containing your survey data. Please make
-sure you read the explanation and that the provided URL is correct. 
-Enjoy! 
+sure you read the explanation and that the provided URL is correct.
+Enjoy!
         """)
         url = input('Put in your URL here:')
-        
+
         if validators.url(url):
-            # Try part requests the data from stated url and checks if the HTTP status
-            # is in the success range 
-            # Uses pandas and to read the content of page and BytesIO to convert bytes
-            # sent from the url in readable format for pandas
+            """
+            Try part requests the data from stated url and checks if the HTTP
+            status is in the success range
+            # Uses pandas and to read the content of page and BytesIO to
+            convert bytes sent from the url in readable format for pandas
+            """
             try:
                 print("Loading your file...\n")
                 response = requests.get(url)
                 response.raise_for_status()
-                
+
                 raw_data = pd.read_csv(BytesIO(response.content))
                 print("The file was loaded successfully!\n")
                 return raw_data
-            
-            # Displays error when something goes wrong with the HTTP request like a wrong
-            # URL or a timeout
+
+            # Displays error when something goes wrong with the HTTP request
+            # like a wrong URL or a timeout
             except requests.exceptions.RequestException as e:
-                print(f"Couldn't download file. Please make sure URL is correct: {e}")
+                print(f"""
+Couldn't download file. Please make sure URL is correct: {e}
+                """)
                 return None
-            
-            # Displays error when file isn't correctly structured or data type is wrong
+
+            # Displays error when file isn't correctly structured or
+            # data type is wrong
             except pd.errors.ParserError as e:
-                print(f"Couldn't read file. Please ensure the file is a CSV file and try again: {e}")
+                print(f"""
+Couldn't read file. Please ensure the file is a CSV file and try again: {e}
+                    """)
                 return None
-            
+
             # Displays all errors that could happen and aren't catched before
             except Exception as e:
                 print(f"Unexpected error: {e}")
                 return None
 
+
 def check_data(data):
     """
-    Checks if the provided data is eligible for analysis. 
+    Checks if the provided data is eligible for analysis.
     Checks for product column and that there is at least one question column.
     Checks that the answers are in the allowed range.
     Removes rows with invalid data and rows with missing data
@@ -60,15 +69,20 @@ def check_data(data):
     print("Checking and cleaning the data...\n")
 
     try:
-        # Checks with two if statements that "Products" column exists and questions exist
+        # Checks with two if statements that "Products" column exists
+        # and questions exist
         if "Product" not in data.columns:
             raise ValueError("The product column is missing, please add one")
         if len(data.columns) < 2:
-            raise ValueError("There are no questions in the data, please add at least one")  
-        
+            raise ValueError("""
+There are no questions in the data, please add at least one
+                            """)
+
         # The allowed answers for the survey can be changed here
-        allowed_answers = ["Very unhappy", "Unhappy", "Neutral", "Happy", "Very happy"]
-        
+        allowed_answers = [
+            "Very unhappy", "Unhappy", "Neutral", "Happy", "Very happy"
+            ]
+
         # Checks that the provided answers are part of the allowed options
         # for every column exept the "Product" column
         for col in data.columns.drop("Product"):
@@ -79,20 +93,23 @@ def check_data(data):
 
         print("Great, your data is now clean and ready for analysis.\n")
         return data
-    
+
     # Returns one of the defined ValueError catched by the two if functions
     except ValueError as e:
         print(f"Error in data: {e}")
         return None
-    
+
     # Returns every unexpected error
     except Exception as e:
         print(f"Unexpected error: {e}")
         return None
 
+
 # This line of code opts-in to a future change how pandas handles downcasting
-# with this line of code the "FutureWarning:.." in the terminal doesn't get shown
+# with this line of code the "FutureWarning:.." doesn't get shown
 pd.set_option('future.no_silent_downcasting', True)
+
+
 def transform_data(data):
     """
     The function transforms text data into numerical values
@@ -102,29 +119,31 @@ def transform_data(data):
     new_data = data.copy()
     for col in data.columns.drop("Product"):
         new_data[col] = new_data[col].replace(
-            ["Very unhappy", "Unhappy", "Neutral", "Happy", "Very happy"], 
+            ["Very unhappy", "Unhappy", "Neutral", "Happy", "Very happy"],
             [1, 2, 3, 4, 5]
         )
     return new_data
 
+
 def return_questions(data):
     """
     This function will count the number of questions in the provided data and
-    return the number of questions asked together with the individual questions 
+    return the number of questions asked together with the individual questions
     to the terminal
     """
     print(f"""
---------------------------------------------------------------------------------    
+--------------------------------------------------------------------------------
 Here is an overview of the data provided:\n
     """)
     number_questions = len(data.columns) - 1
     print(f"Your survey asks {number_questions} different questions:")
-    
+
     i = 1
     for col in data.columns.drop("Product"):
         print(f"Question{i}: {col}")
         i += 1
     print("\n")
+
 
 def count_products(data):
     """
@@ -136,9 +155,11 @@ def count_products(data):
     print("The following products are part of your survey:")
     for product in list_products:
         print(f"Product {counter}: {product}")
-        counter +=1
-    print("\n")    
+        counter += 1
+
+    print("\n")
     return list_products
+
 
 def print_legend():
     """Prints the legend to the console"""
@@ -152,17 +173,19 @@ Legend for results of analysis:
 5 = Very happy\n
     """)
 
+
 def calculate_mean_questions(data):
     """
-    Function to calculate the mean value for the answers given for each question
-    and prints the mean values to the console next to the questions
+    Function to calculate the mean value for the answers given for each
+    question and prints the mean values to the console next to the questions
     """
     mean_questions = data.drop(columns=["Product"]).mean()
-    
+
     print(f"""
 Here are the mean values for each question:
 {mean_questions}\n
     """)
+
 
 def find_mode_questions(data):
     """
@@ -170,12 +193,13 @@ def find_mode_questions(data):
     to the console in a readable and clean manner
     """
     mode_questions = data.drop(columns=["Product"]).mode()
-    # Prints the mode for each question more readable to the console than the 
+    # Prints the mode for each question more readable to the console than the
     # Dataframe that gets returned to mode_questions
     print("Here is the mode for each question:")
     for question, mode in mode_questions.iloc[0].items():
         print(f"{question}: {mode}")
     print("\n")
+
 
 def calculate_mean_products(data, list_products):
     """
@@ -185,15 +209,19 @@ def calculate_mean_products(data, list_products):
     mean_products = {}
     for product in list_products:
         # Creates new dataframe where only rows of the wanted product are left
-        product_row = data[data["Product"] == product].drop(columns=["Product"])
+        product_row = (
+            data[data["Product"] == product]
+            .drop(columns=["Product"])
+        )
         # Calculates the mean of the means of each row
         mean_product = product_row.mean(axis=1).mean()
         mean_products[product] = mean_product
-    
+
     print(f"""
 Here are the mean values for each product:
 {pd.Series(mean_products)}\n
     """)
+
 
 def find_mode_products(data, list_products):
     """
@@ -203,23 +231,27 @@ def find_mode_products(data, list_products):
     print(f"Here is the mode for each product:")
 
     for product in list_products:
-        product_row = data[data["Product"] == product].drop(columns=["Product"])
+        product_row = (
+            data[data["Product"] == product]
+            .drop(columns=["Product"])
+        )
         all_rows = product_row.stack()
         mode_product = all_rows.mode()[0]
         print(f"{product}: {mode_product}")
     print("\n")
 
+
 def start_analysis(num_data, clean_data, list_products):
     """
-    This function analyzes the data and calculates the mean and mode for each 
-    question and product. It also provides a menu where the user can choose to 
+    This function analyzes the data and calculates the mean and mode for each
+    question and product. It also provides a menu where the user can choose to
     watch each analysis individually or to insert a new file for analysis
     """
     quitting = False
     while not quitting:
-        #Deefine parameters for sub menu and back menu/button
+        # Deefine parameters for sub menu and back menu/button
         sub_options = [
-            "[q] Analysis Questions", "[p] Analysis Products", 
+            "[q] Analysis Questions", "[p] Analysis Products",
             "[b] Back to Main Menu"
             ]
         back_sub = ["[b] Back to Sub Menu"]
@@ -248,6 +280,7 @@ def start_analysis(num_data, clean_data, list_products):
         elif sub_menu_choice == "[b] Back to Main Menu":
             quitting = True
 
+
 def main():
     """
     Runs the program and summons all the needed functions
@@ -258,20 +291,20 @@ def main():
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 Hello and welcome to the product-survey-analysis!
-If you already now how it works click on "Start" or 
+If you already now how it works click on "Start" or
 press the "s" button on your keyboard.
-Should it be your first time, click on "Show Explanation" or 
+Should it be your first time, click on "Show Explanation" or
 press the "e" button on your keyboard.
     """)
-    #Definition of options for different terminal menus
+    # Definition of options for different terminal menus
     main_options = ["[s] Start", "[e] Show Explanation", "[q] Quit"]
     back_main = ["[b] Back to Main Menu"]
     quitting = False
     while not quitting:
         # Declares the different terminal menus
         main_menu = TerminalMenu(main_options)
-        back_main_menu = TerminalMenu(back_main)    
-        # Starts the Main Menu 
+        back_main_menu = TerminalMenu(back_main)
+        # Starts the Main Menu
         main_menu_index = main_menu.show()
         main_menu_choice = main_options[main_menu_index]
         # Start the analysis
@@ -280,16 +313,18 @@ press the "e" button on your keyboard.
             raw_data = get_user_input()
             # Check that raw_data is valid before continuing the program
             if raw_data is None:
-                print("An Error occured please check the console and try again.")
+                print("""
+An Error occured please check the console and try again.
+                """)
                 continue
-            else: 
+            else:
                 clean_data = check_data(raw_data)
-                return_questions(clean_data) 
+                return_questions(clean_data)
                 list_products = count_products(clean_data)
                 num_data = transform_data(clean_data)
-                #Analyze the given data
+                # Analyze the given data
                 start_analysis(num_data, clean_data, list_products)
-                
+
         # Shows the explanation how to use the program properly
         elif main_menu_choice == "[e] Show Explanation":
             print(f"""
@@ -297,25 +332,27 @@ press the "e" button on your keyboard.
     stored publicly aviable.
 
 2. The link must provide the raw data, not the document to download.
-    You can use for example github or dropbox and click on "raw data" 
-    to open the right URL. 
+    You can use for example github or dropbox and click on "raw data"
+    to open the right URL.
 
 3. The file needs to be formatted as CSV (Comma-Separated Values).
-    
-4. After running the analyis, the program will then give you an overview for 
+
+4. After running the analyis, the program will then give you an overview for
     your data. It will list the different questions and the products surveyed.
 
-5. You can then over the sub menu look up the analysis for the individual 
-    questions and the individual products. 
+5. You can then over the sub menu look up the analysis for the individual
+    questions and the individual products.
 
-6. To insert a new file, just go back to the main menu and click on start again.
+6. To insert a new file, just go back to the main menu and
+    click on start again.
             """)
-            #Offer a menu/button to go back to main menu
+            # Offer a menu/button to go back to main menu
             back_main_menu_index = back_main_menu.show()
             back_main_menu_choice = back_main[back_main_menu_index]
         # Quits the program
         elif main_menu_choice == "[q] Quit":
             quitting = True
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
